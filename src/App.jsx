7b3d6d35aa2508b8ca48d2eb4929cefd0ae3dd11
Kubanych Asanov <kuba.asanov4@gmail.com>
2022-10-29 23:56:41 +0600

    Deleted vite logo from main page.

--- src/App.jsx
+++ src/App.jsx
@@ -20,14 +20,6 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Kuba Asanov</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -45,9 +37,6 @@ function App() {
         rows={5}
       />
     </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
